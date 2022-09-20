0dd66989245ac09c8590aef4e086f01cb0beb880
Dylan Frosini <dylan.frosini@outlook.com>
2022-09-20 17:51:56 +0200

    Projektdateien hinzufügen.

---
+++ MyChatApp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using ModernChat.MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyChatApp.MVVM.ViewModel {
+    internal class MainViewModel {
+        public ObservableCollection<MessageModel> Messages { get; set; }
+
+        public ObservableCollection<ContactModel> Contacts { get; set; }
+
+
+        public MainViewModel() { 
+        
+            Messages = new ObservableCollection<MessageModel>();
+            Contacts = new ObservableCollection<ContactModel>();
+
+            for (int i = 0; i < 5; i++) {
+                Contacts.Add(new ContactModel());
+                Contacts[i].Username = "Martin";
+            }
+        
+        }
+    }
+}
